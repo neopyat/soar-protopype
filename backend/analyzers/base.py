0159741972f5a94e64d7bd81ed732019cab0745f
neopyat <sarvarashurov2904@gmail.com>
2026-05-05 02:37:13 +0500

    нужно в этой версии исправить ошибки с мл-модулем и пайплайном

--- backend/analyzers/base.py
+++ backend/analyzers/base.py
@@ -1,11 +1,15 @@
 from abc import ABC, abstractmethod
-from typing import List
+from typing import Optional
 
 from models.event import Event
 from models.incident import Incident
 
 
 class BaseAnalyzer(ABC):
+
     @abstractmethod
-    def analyze(self, events: List[Event]) -> List[Incident]:
-        ...
+    def analyze(self, event: Event) -> Optional[Incident]:
+        """
+        Принимает одно событие и возвращает либо инцидент, либо None
+        """
+        pass
