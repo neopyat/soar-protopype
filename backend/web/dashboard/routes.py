d97387b802e5973386209b1bcaade6598d01b3f9
neopyat <sarvarashurov2904@gmail.com>
2026-05-07 23:23:38 +0500

    поправил проблему с дэшбордом

--- backend/web/dashboard/routes.py
+++ backend/web/dashboard/routes.py
@@ -1,5 +1,9 @@
 from flask import Blueprint, render_template
+
 from flask_login import login_required, current_user
+
+from web.models.incident import Incident
+
 
 dashboard_bp = Blueprint("dashboard", __name__)
 
@@ -7,7 +11,18 @@ dashboard_bp = Blueprint("dashboard", __name__)
 @dashboard_bp.route("/")
 @login_required
 def index():
+    incidents = (
+        Incident.query
+        .order_by(Incident.timestamp.desc())
+        .limit(20)
+        .all()
+    )
+
+    total_incidents = Incident.query.count()
+
     return render_template(
         "dashboard/index.html",
-        username=current_user.username
+        username=current_user.username,
+        incidents=incidents,
+        total_incidents=total_incidents
     )
--- backend/web/models/incident.py
+++ backend/web/models/incident.py
@@ -1,22 +1,21 @@
-from datetime import datetime
-
 from web.extensions import db
 
 
 class Incident(db.Model):
     __tablename__ = "incidents"
 
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.String, primary_key=True)
 
     type = db.Column(db.String(100), nullable=False)
-    ip = db.Column(db.String(100), nullable=True)
 
-    severity = db.Column(db.String(20), default="low")
-    status = db.Column(db.String(20), default="open")
+    ip = db.Column(db.String(100))
 
-    mitre = db.Column(db.String(50), nullable=True)
-    threat = db.Column(db.String(50), nullable=True)
+    severity = db.Column(db.String(20))
 
-    raw_log = db.Column(db.Text, nullable=True)
+    timestamp = db.Column(db.Float)
 
-    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    status = db.Column(db.String(20))
+
+    mitre = db.Column(db.String(50))
+
+    threat = db.Column(db.String(50))
